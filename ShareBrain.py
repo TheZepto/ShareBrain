f0fd5ab47ec6eb38f21aa8966d3c39071f7aeddc
Dane Laban <dane.laban@gmail.com>
2017-01-05 19:08:28 +1000

    Customised script to include days_of_data variable (determines input size of neural network)

--- ShareBrain.py
+++ ShareBrain.py
@@ -38,34 +38,43 @@ except ValueError:
 	quit()
 
 #Take the historical data and form a training set for the neural net.
-#Each training example has 2 linear inputs: the opening share price of the day
-#and the previous day; and 1 binary output: whether the opening share price
-#increased on the day after.
+#Each training example has 3 linear inputs: the opening share price of the day
+#and the previous day and the volume of the day; and 1 binary output: whether 
+#the opening share price increased on the day after.
+days_of_data = 20 #Set how many inputs are used in the network for open price
+
 training_input = np.array([])
 training_target = np.array([])
 
-for i in range(1, len(open_price)-1):
-	training_input = np.append(training_input, [open_price[i-1:i+1]])
-	training_target = np.append(training_target, [open_price[i+1] > open_price[i]] )
+for i in range(0, len(open_price)-days_of_data):
+	training_input = np.append(training_input, open_price[i:i+days_of_data])
+	training_input = np.append(training_input, volume[i])
+	training_target = np.append(training_target, open_price[i+days_of_data-1] > open_price[i+days_of_data] )
+
 
 #The above for loop makes 1-dim arrays with the values in them. Need to use reshape
 #on the input and training data to make it 2-dim. The -1 in reshape will be filled 
-#automatically and the 2 is there as there should be 2 columns for each input. Likewise
+#automatically and the 2 is there as there should be 3 columns for each input. Likewise
 #for the target array.
-training_input = np.reshape(training_input, (-1,2))
+
+training_input = np.reshape(training_input, (-1, days_of_data+1))
 training_target = np.reshape(training_target, (-1,1))
 
 #Setting up the neural network as a single layer perceptron. Need to work out the min
 #and max of each training datatype and use them to define each input to the neural net.
-training_input_min = np.min(training_input)
-training_input_max = np.max(training_input)
-minmax = [training_input_min, training_input_max]
+open_minmax = [np.min(open_price), np.max(open_price)]
+volume_minmax = [np.min(volume), np.max(volume)]
 
-net = nl.net.newp([ minmax, minmax], 1)
+minmax = []
+for i in range(0,days_of_data):
+	minmax.append(open_minmax)
+minmax.append(volume_minmax)
 
-#Train the neural network using delta trainer with 100 epochs (displaying every 25)
+net = nl.net.newp(minmax, 1)
+
+#Train the neural network using delta trainer with 100 epochs (displaying every 10)
 #with a learning rate of 0.1
-training_error = net.train(training_input, training_target, epochs=100, show=25)
+training_error = net.train(training_input, training_target, epochs=100, show=10)
 
 # # Plot results
 # import pylab as pl
@@ -78,4 +87,4 @@ training_error = net.train(training_input, training_target, epochs=100, show=25)
 #Evaluate the networks performance on the training data
 simulated_target = net.sim(training_input)
 correct_predictions = np.sum( np.equal(simulated_target, training_target) ) / len(simulated_target) * 100
-print("The network is {0:.2f}% correct".format(correct_predictions))
+print("The network is {0:.2f}% correct from the training data.".format(correct_predictions))
