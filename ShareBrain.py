4f903184216f2343cb5422387853825dddf65adc
Dane Laban <dane.laban@gmail.com>
2017-03-24 16:57:59 +1000

    Implemented logical training input method. No better than 50% accuracy

--- ShareBrain.py
+++ ShareBrain.py
@@ -30,31 +30,30 @@ historical_data = sharescraper.get_share_data(
 	end_date='2016-01-01',
 	use_existing_data=True)
 
-(price_input, boolean_target) = sharescraper.proc_share_bool_target(
+(boolean_input, boolean_target) = sharescraper.proc_share_bool_inp_targ(
 	historical_data,
-	days_of_data=10)
+	days_of_data=30)
 
 # Separate data into training set and test set
 random_number = 0
 test_split = 0.3
 X_train, X_test, y_train, y_test = train_test_split(
-	price_input, boolean_target, test_size=test_split, random_state=random_number)
+	boolean_input, boolean_target, test_size=test_split, random_state=random_number)
 
-# Feature scale the training data and apply the scaling to the training and test datasets.
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+# # Feature scale the training data and apply the scaling to the training and test datasets.
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+# X_train = scaler.transform(X_train)
+# X_test = scaler.transform(X_test)
 
 # Set up the MLPClassifier
 clf = MLPClassifier(
 	activation = 'tanh',
-	learning_rate = 'adaptive',
 	solver ='adam',
-	hidden_layer_sizes=(10),
-	alpha = 0.01,
-	max_iter = 10000,
-	tol = 1E-8,
+	hidden_layer_sizes=(1000,500,100,50,20,10,5),
+	alpha = 1,
+	max_iter = 300,
+	tol = 1E-5,
 	warm_start = False,
 	verbose = True )
 
@@ -71,142 +70,99 @@ while accuracy_check:
 		.format(test_accuracy*100, train_accuracy*100))
 	accuracy_check = test_accuracy < 0.1
 
-# Build an array of indices to order the train and test data back in the plot
-indices = np.arange(len(boolean_target))
-i_train, i_test, iy_train, iy_test = train_test_split(
-	indices, indices, test_size=test_split, random_state=random_number)
+# # Build an array of indices to order the train and test data back in the plot
+# indices = np.arange(len(boolean_target))
+# i_train, i_test, iy_train, iy_test = train_test_split(
+# 	indices, indices, test_size=test_split, random_state=random_number)
 
-# Extract closing price data for test and training data
-# rescaled back to original price
-close_price_test = scaler.inverse_transform(X_test)[:,-1]
-close_price_train = scaler.inverse_transform(X_train)[:,-1]
+# # Extract closing price data for test and training data
+# # rescaled back to original price
+# close_price_test = scaler.inverse_transform(X_test)[:,-1]
+# close_price_train = scaler.inverse_transform(X_train)[:,-1]
 
-# Generate the predictions for the test data
-predictions = clf.predict(X_test)
-predictions_prob = clf.predict_proba(X_test)
+# # Generate the predictions for the test data
+# predictions = clf.predict(X_test)
+# predictions_prob = clf.predict_proba(X_test)
 
-# Separate the correct and incorrect predictions into their own arrays
-true_pos = []
-true_pos_prob = []
-i_true_pos = []
-true_neg = []
-true_neg_prob = []
-i_true_neg = []
-false_pos = []
-false_pos_prob = []
-i_false_pos = []
-false_neg = []
-false_neg_prob = []
-i_false_neg = []
+# # Separate the correct and incorrect predictions into their own arrays
+# true_pos = []
+# true_pos_prob = []
+# i_true_pos = []
+# true_neg = []
+# true_neg_prob = []
+# i_true_neg = []
+# false_pos = []
+# false_pos_prob = []
+# i_false_pos = []
+# false_neg = []
+# false_neg_prob = []
+# i_false_neg = []
 
-for n in range(0,len(predictions)):
-	if predictions[n] == True and y_test[n] == True:
-		true_pos = np.append(true_pos, close_price_test[n])
-		true_pos_prob = np.append(true_pos_prob, predictions_prob[n,1])
-		i_true_pos = np.append(i_true_pos, i_test[n])
-	if predictions[n] == False and y_test[n] == False:
-		true_neg = np.append(true_neg, close_price_test[n])
-		true_neg_prob = np.append(true_neg_prob, predictions_prob[n,0])
-		i_true_neg = np.append(i_true_neg, i_test[n])
-	if predictions[n] == True and y_test[n] == False:
-		false_pos = np.append(false_pos, close_price_test[n])
-		false_pos_prob = np.append(false_pos_prob, predictions_prob[n,1])
-		i_false_pos = np.append(i_false_pos, i_test[n])
-	if predictions[n] == False and y_test[n] == True:
-		false_neg = np.append(false_neg, close_price_test[n])
-		false_neg_prob = np.append(false_neg_prob, predictions_prob[n,0])
-		i_false_neg = np.append(i_false_neg, i_test[n])
+# for n in range(0,len(predictions)):
+# 	if predictions[n] == True and y_test[n] == True:
+# 		true_pos = np.append(true_pos, close_price_test[n])
+# 		true_pos_prob = np.append(true_pos_prob, predictions_prob[n,1])
+# 		i_true_pos = np.append(i_true_pos, i_test[n])
+# 	if predictions[n] == False and y_test[n] == False:
+# 		true_neg = np.append(true_neg, close_price_test[n])
+# 		true_neg_prob = np.append(true_neg_prob, predictions_prob[n,0])
+# 		i_true_neg = np.append(i_true_neg, i_test[n])
+# 	if predictions[n] == True and y_test[n] == False:
+# 		false_pos = np.append(false_pos, close_price_test[n])
+# 		false_pos_prob = np.append(false_pos_prob, predictions_prob[n,1])
+# 		i_false_pos = np.append(i_false_pos, i_test[n])
+# 	if predictions[n] == False and y_test[n] == True:
+# 		false_neg = np.append(false_neg, close_price_test[n])
+# 		false_neg_prob = np.append(false_neg_prob, predictions_prob[n,0])
+# 		i_false_neg = np.append(i_false_neg, i_test[n])
 
-#Calculate precision, recall and F1 scores and display them
-try:	
-	precision = len(true_pos) / (len(true_pos) + len(false_pos))
-	recall = len(true_pos) / (len(true_pos) + len(false_neg))
-	F1_score = 2 * precision * recall / (precision + recall)
-except:
-	precision = 0
-	recall = 0
-	F1_score = 0
+# #Calculate precision, recall and F1 scores and display them
+# try:	
+# 	precision = len(true_pos) / (len(true_pos) + len(false_pos))
+# 	recall = len(true_pos) / (len(true_pos) + len(false_neg))
+# 	F1_score = 2 * precision * recall / (precision + recall)
+# except:
+# 	precision = 0
+# 	recall = 0
+# 	F1_score = 0
 
-print("The precision is {:.3f}, the recall is {:.3f}, and the F1 score is {:.3f}."
-	.format(precision, recall, F1_score))
+# print("The precision is {:.3f}, the recall is {:.3f}, and the F1 score is {:.3f}."
+# 	.format(precision, recall, F1_score))
 
-# Plot the test and training data
-fig, (ax1, ax2) = plt.subplots(2,1)
-ax1.plot(i_train, close_price_train,'b.')
-ax1.plot(i_test, close_price_test,'m.')
+# # Plot the test and training data
+# fig, (ax1, ax2) = plt.subplots(2,1)
+# ax1.plot(i_train, close_price_train,'b.')
+# ax1.plot(i_test, close_price_test,'m.')
 
-# Shade the regions where the predictions are made
-# This is set up to colour the next price data point from when the prediction is made
-# The shaded area starts from the middle of the graph and extends upwards when
-# the price actually increased and extends downwards when the true price decreased.
-# Red indicates the prediction was incorrect and green for correct.
-graph_top = np.max(np.append(close_price_test,close_price_train))+2
-graph_bottom = np.min(np.append(close_price_test,close_price_train))-2
-graph_middle = 0.5*(graph_top+graph_bottom)
+# # Shade the regions where the predictions are made
+# # This is set up to colour the next price data point from when the prediction is made
+# # The shaded area starts from the middle of the graph and extends upwards when
+# # the price actually increased and extends downwards when the true price decreased.
+# # Red indicates the prediction was incorrect and green for correct.
+# graph_top = np.max(np.append(close_price_test,close_price_train))+2
+# graph_bottom = np.min(np.append(close_price_test,close_price_train))-2
+# graph_middle = 0.5*(graph_top+graph_bottom)
 
-for n in range(0,len(true_pos)):
-	ax1.fill_between(i_true_pos[n]+[0.5, 1.5], graph_middle, 100, facecolor='g', linewidth=0)
+# for n in range(0,len(true_pos)):
+# 	ax1.fill_between(i_true_pos[n]+[0.5, 1.5], graph_middle, 100, facecolor='g', linewidth=0)
 
-for n in range(0,len(true_neg)):
-	ax1.fill_between(i_true_neg[n]+[0.5, 1.5], 0, graph_middle, facecolor='g', linewidth=0)
+# for n in range(0,len(true_neg)):
+# 	ax1.fill_between(i_true_neg[n]+[0.5, 1.5], 0, graph_middle, facecolor='g', linewidth=0)
 
-for n in range(0,len(false_pos)):
-	ax1.fill_between(i_false_pos[n]+[0.5, 1.5], graph_middle, 100, facecolor='r', linewidth=0)
+# for n in range(0,len(false_pos)):
+# 	ax1.fill_between(i_false_pos[n]+[0.5, 1.5], graph_middle, 100, facecolor='r', linewidth=0)
 
-for n in range(0,len(false_neg)):
-	ax1.fill_between(i_false_neg[n]+[0.5, 1.5], 0, graph_middle, facecolor='r', linewidth=0)
+# for n in range(0,len(false_neg)):
+# 	ax1.fill_between(i_false_neg[n]+[0.5, 1.5], 0, graph_middle, facecolor='r', linewidth=0)
 
-# Plot the prediction uncertainty on the second axis
-ax2.plot(i_true_pos, true_pos_prob,'g+')
-ax2.plot(i_true_neg, true_neg_prob, 'g.')
-ax2.plot(i_false_pos, false_pos_prob, 'r+')
-ax2.plot(i_false_neg, false_neg_prob, 'r.')
+# # Plot the prediction uncertainty on the second axis
+# ax2.plot(i_true_pos, true_pos_prob,'g+')
+# ax2.plot(i_true_neg, true_neg_prob, 'g.')
+# ax2.plot(i_false_pos, false_pos_prob, 'r+')
+# ax2.plot(i_false_neg, false_neg_prob, 'r.')
 
-# Display the plot
-ax1.set_ylim(bottom=graph_bottom , top=graph_top)
-ax1.set_xlim(left=0, right= len(boolean_target))
-ax2.set_xlim(left=0, right= len(boolean_target))
-plt.show()
-
-# # Use the network to predict earning
-# (price_input_sim, boolean_target_sim) = sharescraper.get_share_data_boolean_target(
-# 	share_name='ANZ.AX',
-# 	start_date='2016-01-01',
-# 	end_date='2016-03-01',
-# 	days_of_data=15,
-# 	use_existing_data=False)
-
-# # Build the input array for the first day of prediction
-# X_today = price_input[-1,:]
-# X_today = np.reshape(X_today,(1,-1))
-
-# #Initialise the simulation variables
-# shares_owned = False
-# profit = 0
-# buy_prob = 0.99
-# sell_prob = 0.99
-
-# #Begin the market simulation
-# for n in range(0, len(boolean_target_sim)):
-# 	X_today = np.append(X_today[:,-28:], price_input_sim[n,-2:])
-# 	X_today = np.reshape(X_today,(1,-1))
-
-# 	X_scaled = scaler.transform(X_today)
-# 	import pdb; pdb.set_trace()
-# 	pred_today = clf.predict(X_scaled)
-# 	if pred_today:
-# 		pred_today_err = clf.predict_proba(X_scaled)[0,1]
-# 	else:
-# 		pred_today_err = clf.predict_proba(X_scaled)[0,0]
-
-# 	# Buy 1 share is the price is predicted to increase, we don't already
-# 	# own a share, and the prediction probability is greater than the limit
-# 	if pred_today == True and shares_owned == False and pred_today_err >= buy_prob :
-# 		profit = profit - X_today[:,-1]
-# 		shares_owned = True
-
-# 	if pred_today == False and shares_owned == True and pred_today_err >= sell_prob :
-# 		profit = profit + X_today[:,-1]
-# 		shares_owned = False
-
-# 	print(X_today[:,-1], profit, pred_today, pred_today_err)
+# # Display the plot
+# ax1.set_ylim(bottom=graph_bottom , top=graph_top)
+# ax1.set_xlim(left=0, right= len(boolean_target))
+# ax2.set_xlim(left=0, right= len(boolean_target))
+# plt.show()
--- sharescraper.py
+++ sharescraper.py
@@ -99,3 +99,52 @@ def proc_share_bool_target(
 	training_target = np.reshape(training_target, (-1,))
 	
 	return (training_input, training_target)
+
+# Process the historical share data with boolean training input target
+# Using a logical input of close_price tomorrow > close_price today. The training target
+# is the same logical value but for the future value outside of the range.
+# Inputs
+# - historical_data as returned from get_share_data 
+# - days_of_data is the number of consecutive days to be converted into inputs
+# Returns
+# - training_input array with the boolean values for tomorrow > today in
+#   the number of days specified in days_of_data between the start_date and end_date.
+# - training_target array consist of a boolean value indicating if the closing price 
+#   tomorrow is greater than the closing price today.
+def proc_share_bool_inp_targ(
+	historical_data,
+	days_of_data=30):
+	
+	# Process the returned data into 3 lists of: open_price, close_price, and volume
+	try:
+		close_price = [float(historical_data[i]['Close']) for i in range(0,len(historical_data)) ]
+
+	except ValueError:
+		print("Error in processing share data.")
+		quit()
+
+	# Take the historical data and form a training set for the neural net.
+	# Each training example is a boolean value indicating if the price tomorrow is
+	# greater than today for each day in days_of_data
+	# The output is boolean indicating if the close price tomorrow is greater than today.
+
+	training_input = np.array([])
+	training_target = np.array([])
+
+	training_example_number = len(close_price) - days_of_data
+
+	for i in range(0, training_example_number):
+		boolean_input = [close_price[i+n+1] > close_price[i+n] for n in range(0, days_of_data-1)]
+		training_input = np.append(training_input, boolean_input)
+		training_target = np.append(training_target, close_price[i+days_of_data] > close_price[i+days_of_data-1] )
+
+	# The above for loop makes 1-dim arrays with the values in them. Need to use reshape
+	# on training input to make it 2-dim. Number of columns is 3*days_of_data to account for 
+	# open price, close price and volume. The -1 in reshape will be filled 
+	# automatically and (days_of_data +1) is the number of columns for each input. Likewise
+	# for the target array.
+
+	training_input = np.reshape(training_input, (-1, days_of_data-1))
+	training_target = np.reshape(training_target, (-1,))
+	
+	return (training_input, training_target)
